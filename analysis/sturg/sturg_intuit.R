7fa990bf1866f8351dd2b9dd722407fdd6d522c5
David Gerard <gerard.1787@gmail.com>
2022-05-02 10:16:30 -0400

    remove commented code

--- analysis/sturg/sturg_intuit.R
+++ analysis/sturg/sturg_intuit.R
@@ -64,14 +64,11 @@ for (i in seq_len(nrow(pdf))) {
   pdf$chisq_dr[[i]] <- (obs - ex)^2 / ex
 }
 
-
 pdf %>%
   filter(naive < 0.05) %>%
   mutate(diff = ustat - naive) %>%
   arrange(desc(diff)) ->
   pdf_sub
-
-
 
 pdf_sub %>%
   slice(1:15) %>%
@@ -138,58 +135,7 @@ ggplot() +
   pltab
 ggsave(filename = "./output/sturg/chisq_tab.pdf", plot = pltab, height = 4, width = 4, family = "Times")
 
-
 i <- 5
 pdf_sub$n[[i]]
 round(pdf_sub$chisq_naive[[i]], digits = 2)
 round(pdf_sub$chisq_dr[[i]], digits = 2)
-
-## Explore general properties
-# nind <- 19
-# nrep <- 10000
-# ploidy <- 4
-# q1 <- hwep::hwefreq(r = 0.5, alpha = 0, ploidy = ploidy, niter = Inf)
-# q2 <- hwep::hwefreq(r = 0.5, alpha = drbounds(ploidy), ploidy = ploidy, niter = Inf)
-#
-# rdiv <- function(n, size, prob1, prob2) {
-#   stopifnot(length(prob1) == length(prob2))
-#   ploidy <- length(prob1) - 1
-#   outmat <- matrix(NA_real_, ncol = ploidy + 1, nrow = n)
-#   for (i in seq_len(n)) {
-#     isone <- sample(c(TRUE, FALSE), size = 1)
-#     if (isone) {
-#       outmat[i, ] <- c(stats::rmultinom(n = 1, size = size, prob = prob1))
-#     } else {
-#       outmat[i, ] <- c(stats::rmultinom(n = 1, size = size, prob = prob2))
-#     }
-#   }
-#   return(outmat)
-# }
-#
-# n1 <- t(stats::rmultinom(n = nrep, size = nind, prob = q1))
-# n2 <- rdiv(n = nrep, size = nind, prob1 = q1, prob2 = q2)
-#
-# colnames(n1) <- 0:ploidy
-# colnames(n2) <- 0:ploidy
-#
-# as_tibble(n1) %>%
-#   mutate(Distribution = "Binomial") ->
-#   n1df
-#
-# as_tibble(n2) %>%
-#   mutate(Distribution = "Mixture") ->
-#   n2df
-#
-# bind_rows(n1df, n2df) %>%
-#   gather(-Distribution, key = "Genotype", value = "Count") %>%
-#   count(Distribution, Genotype, Count) %>%
-#   group_by(Distribution, Genotype) %>%
-#   mutate(freq = n / sum(n)) %>%
-#   ggplot(aes(x = Count, y = freq, fill = Distribution)) +
-#   geom_col(position = "dodge") +
-#   facet_wrap(.~Genotype) +
-#   theme_bw() +
-#   theme(strip.background = element_rect(fill = "white")) +
-#   scale_fill_colorblind() +
-#   ylab("Pr(Genotype)") +
-#   xlab("Genotype")
